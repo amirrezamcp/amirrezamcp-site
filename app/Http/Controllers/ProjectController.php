e8cdf95db0e6d3e3c5654fbe3954fadd9425dc7b
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-06-06 19:13:05 +0330

    Update ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,26 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        $project = Project::all();
-        if($project->isEmpty()) {
-            $response = [
+        $projects = Project::all();
+
+        if($projects->isEmpty()) {
+            return response()->json([
                 'status' => false,
-                'message' => 'not project'
-            ];
-            return response()->json($response, 401);
+                'message' => 'پروژه‌ای وجود ندارد'
+            ], 404);
         }
 
         return response()->json([
-            "status" => true,
-            "message" => "project information",
-            "data" => $project
+            'status' => true,
+            'message' => 'پروژه‌ها با موفقیت بارگذاری شدند',
+            'data' => $projects
         ]);
+    }
+
+    public function store(ProjectRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $project = Project::create($data);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'پروژه با موفقیت ثبت شد',
+            'data' => $project,
+        ], 201);
     }
 }
