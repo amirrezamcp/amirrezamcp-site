21ed2efc0ae5bdccf647e12aa4b12d6f45055412
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-06-06 18:57:18 +0330

    Update MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,64 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MessageRequest;
 use App\Models\Message;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class MessageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function store(MessageRequest $request): JsonResponse
     {
-        //
-    }
+        $data = $request->validated();
+        $message = Message::create($data);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Message $message)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Message $message)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Message $message)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Message $message)
-    {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'پیام شما با موفقیت ارسال شد.',
+            'data' => $message,
+        ], 201);
     }
 }
